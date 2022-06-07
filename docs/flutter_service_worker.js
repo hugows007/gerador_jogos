53650428cfff2cba871d8da5cd056b86c712fbe6
hugows007 <hugobezerrapimentel@gmail.com>
2022-06-06 23:15:28 -0300

    [PAGES]

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -16,9 +16,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0958ce9774a0df06ebeeea111e492f2e",
-"/": "0958ce9774a0df06ebeeea111e492f2e",
-"main.dart.js": "3a3879f29dc1907b4d626e26e25c9d6c",
+"index.html": "5f127be0f972a0128ae666ce03c69bd5",
+"/": "5f127be0f972a0128ae666ce03c69bd5",
+"main.dart.js": "9addbbc362a839bdcf577dcc4555d2f7",
 "manifest.json": "0324ad2653d663096a0b5465309c862a",
 "version.json": "beb4b433191fcb86e4d6d49ff2fb3879"
 };
